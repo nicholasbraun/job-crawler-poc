9d214ec832638ead93566db7cddbdef72947b19d
Nicholas Braun <hallo@nicholasbraun.de>
2026-03-10 20:13:36 +0100

    feat: add in memory frontier

--- internal/frontier/inmem/inmem.go
+++ internal/frontier/inmem/inmem.go
@@ -1,3 +1,140 @@
 // Package inmem is the adapter for the in-memory url frontier.
 // This is where the logic is implemented
 package inmem
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	crawler "github.com/nicholasbraun/job-crawler-poc/internal"
+)
+
+type queue struct {
+	deadline time.Time
+	cooldown time.Duration
+	urls     []crawler.URL
+}
+
+func newQueue(cooldown time.Duration) *queue {
+	return &queue{
+		deadline: time.Now(),
+		cooldown: cooldown,
+		urls:     []crawler.URL{},
+	}
+}
+
+// push adds an element to the queue
+func (q *queue) push(url crawler.URL) {
+	q.urls = append(q.urls, url)
+}
+
+// pop returns the first element from the queue and removes it from the queue
+func (q *queue) pop() (crawler.URL, bool) {
+	if len(q.urls) > 0 {
+		url := q.urls[0]
+		q.urls = q.urls[1:]
+
+		return url, true
+	}
+
+	return crawler.URL{}, false
+}
+
+type FrontierOption func(*Frontier)
+
+type Frontier struct {
+	queues   map[string]*queue
+	cooldown time.Duration
+	mu       sync.Mutex
+	signal   chan struct{}
+}
+
+func WithCooldown(c time.Duration) FrontierOption {
+	return func(f *Frontier) {
+		f.cooldown = c
+	}
+}
+
+func NewFrontier(opts ...FrontierOption) *Frontier {
+	f := &Frontier{
+		queues:   map[string]*queue{},
+		cooldown: time.Second,
+		mu:       sync.Mutex{},
+		signal:   make(chan struct{}, 1),
+	}
+
+	for _, fn := range opts {
+		fn(f)
+	}
+
+	return f
+}
+
+func (f *Frontier) AddURL(ctx context.Context, url crawler.URL) error {
+	f.mu.Lock()
+
+	if _, ok := f.queues[url.Base]; !ok {
+		f.queues[url.Base] = newQueue(f.cooldown)
+	}
+
+	f.queues[url.Base].push(url)
+
+	f.mu.Unlock()
+
+	select {
+	case f.signal <- struct{}{}:
+	default:
+	}
+
+	return nil
+}
+
+func (f *Frontier) Next(ctx context.Context) (crawler.URL, error) {
+	for {
+		f.mu.Lock()
+
+		var nextDeadline *time.Time
+
+		for _, q := range f.queues {
+			if len(q.urls) == 0 {
+				continue
+			}
+
+			if time.Until(q.deadline) <= 0 {
+				url, _ := q.pop()
+				q.deadline = time.Now().Add(f.cooldown)
+				f.mu.Unlock()
+				return url, nil
+			}
+
+			if nextDeadline == nil || q.deadline.Before(*nextDeadline) {
+				nextDeadline = &q.deadline
+			}
+		}
+
+		f.mu.Unlock()
+
+		// there are still URLs in some of the queues,
+		// wait until the next deadline is ready or a signal that a new url got added
+		if nextDeadline != nil {
+			select {
+			case <-f.signal:
+				continue
+			case <-time.After(time.Until(*nextDeadline)):
+				continue
+			case <-ctx.Done():
+				return crawler.URL{}, errors.New("still urls left, but context done")
+			}
+		}
+
+		// no urls left in the queues, we are waiting for a signal that a new one got added
+		select {
+		case <-f.signal:
+			continue
+		case <-ctx.Done():
+			return crawler.URL{}, errors.New("no urls found, and context done")
+		}
+	}
+}
---
+++ internal/frontier/inmem/inmem_test.go
@@ -0,0 +1,81 @@
+package inmem_test
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+
+	crawler "github.com/nicholasbraun/job-crawler-poc/internal"
+	"github.com/nicholasbraun/job-crawler-poc/internal/frontier/inmem"
+)
+
+func TestFrontierAddAndRetrieveSingleURL(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		frontier := inmem.NewFrontier(inmem.WithCooldown(time.Second))
+
+		url := crawler.URL{Base: "base", Path: "path", Depth: 1}
+		err := frontier.AddURL(t.Context(), url)
+		if err != nil {
+			t.Fatalf("error adding URL to frontier. error: %v", err)
+		}
+
+		nextURL, err := frontier.Next(t.Context())
+		if err != nil {
+			t.Fatalf("error getting the next URL from frontier. error: %v", err)
+		}
+
+		if nextURL != url {
+			t.Errorf("nextUrl and url should be the same. nextUrl: %v", nextURL)
+		}
+	})
+}
+
+func TestFrontierCooldown(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		cooldown := time.Second
+		frontier := inmem.NewFrontier(inmem.WithCooldown(cooldown))
+
+		url1 := crawler.URL{Base: "base", Path: "path1", Depth: 1}
+		url2 := crawler.URL{Base: "base", Path: "path2", Depth: 1}
+		err := frontier.AddURL(t.Context(), url1)
+		if err != nil {
+			t.Fatalf("error adding URL1 to frontier. error: %v", err)
+		}
+		err = frontier.AddURL(t.Context(), url2)
+		if err != nil {
+			t.Fatalf("error adding URL2 to frontier. error: %v", err)
+		}
+
+		nextURL, err := frontier.Next(t.Context())
+		if err != nil {
+			t.Fatalf("error getting the next URL from frontier. error: %v", err)
+		}
+
+		if nextURL != url1 {
+			t.Errorf("url1 should be the next url. it is %v", nextURL)
+		}
+
+		urlChan := make(chan crawler.URL, 1)
+
+		go func() {
+			nextURL2, err2 := frontier.Next(t.Context())
+			if err2 != nil {
+				t.Errorf("error getting the next URL from frontier. error: %v", err2)
+			}
+
+			urlChan <- nextURL2
+		}()
+
+		synctest.Wait()
+
+		time.Sleep(cooldown)
+
+		synctest.Wait()
+
+		nextURL2 := <-urlChan
+
+		if nextURL2 != url2 {
+			t.Errorf("url2 should be the next url. it is %v", nextURL2)
+		}
+	})
+}
