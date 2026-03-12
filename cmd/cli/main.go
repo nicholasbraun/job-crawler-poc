fbf26da8828d415acb17f43c87b1e7d518957faf
Nicholas Braun <hallo@nicholasbraun.de>
2026-03-12 23:06:17 +0100

    feat: add orchestrator and main.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,83 @@
 // Package main is responsible for:
 //
-// - parsng args (seed urls, depth, ...?)
+// - parsng args (seed urls)
 // - running the crawl orchestrator
 package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	crawler "github.com/nicholasbraun/job-crawler-poc/internal"
+	"github.com/nicholasbraun/job-crawler-poc/internal/database/sqlite"
+	"github.com/nicholasbraun/job-crawler-poc/internal/filter"
+	"github.com/nicholasbraun/job-crawler-poc/internal/frontier/inmem"
+	"github.com/nicholasbraun/job-crawler-poc/internal/http"
+	"github.com/nicholasbraun/job-crawler-poc/internal/orchestrator"
+	"github.com/nicholasbraun/job-crawler-poc/internal/parser"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// seed URLs from command line args
+	seedURLsPtr := flag.String("seedURLs", "https://news.ycombinator.com/jobs", "comma separated list of seedURLs")
+	dbPath := flag.String("db", "./data/database.db", "path to sqlite database")
+	flag.Parse()
+
+	seedURLs := strings.Split(*seedURLsPtr, ",")
+
+	// create SQLite DB + setup
+	db, err := sqlite.Open(*dbPath)
+	if err != nil {
+		log.Fatalf("error opening db with path: %s. %v", *dbPath, err)
+	}
+	err = sqlite.Setup(ctx, db)
+	if err != nil {
+		log.Fatalf("error setting up db: %v", err)
+	}
+
+	// create repositories
+	urlRepository := sqlite.NewURLRepository(db)
+	jobRepository := sqlite.NewJobRepository(db)
+
+	// create frontier
+	frontier := inmem.NewFrontier(inmem.WithMaxDomains(10))
+
+	// create HTTP client + retry wrapper
+	httpClient := http.NewClient()
+	retryHTTPClient := http.NewRetryClient(httpClient)
+
+	// create parser
+	parser := parser.NewHTMLParser()
+
+	// create filter chains (start with pass-through filters)
+	contentFilter := filter.Chain[*crawler.Content]() // empty chain = pass everything
+	urlFilter := filter.Chain[*crawler.URL]()
+	relevanceFilter := filter.Chain[*crawler.Content]()
+
+	// create orchestrator
+	cfg := orchestrator.Config{
+		Frontier:        frontier,
+		Downloader:      retryHTTPClient,
+		Parser:          parser,
+		URLRepository:   urlRepository,
+		JobRepository:   jobRepository,
+		ContentFilter:   contentFilter,
+		URLFilter:       urlFilter,
+		RelevanceFilter: relevanceFilter,
+	}
+	o := orchestrator.NewOrchestrator(cfg)
+
+	// run
+	err = o.Run(ctx, seedURLs)
+	if err != nil {
+		log.Fatalf("crawl failed: %v", err)
+	}
+	log.Println("crawl complete")
+}
--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -1,3 +1,157 @@
 // Package orchestrator is where everything is tied together.
 // This is the entrypoint for our cmd/*/main.go.
 package orchestrator
+
+import (
+	"context"
+	"log/slog"
+
+	crawler "github.com/nicholasbraun/job-crawler-poc/internal"
+	"github.com/nicholasbraun/job-crawler-poc/internal/filter"
+	"github.com/nicholasbraun/job-crawler-poc/internal/frontier"
+	"github.com/nicholasbraun/job-crawler-poc/internal/http"
+	"github.com/nicholasbraun/job-crawler-poc/internal/parser"
+)
+
+type Config struct {
+	Frontier        frontier.Frontier
+	Downloader      http.Downloader
+	Parser          parser.Parser
+	URLRepository   crawler.URLRepository
+	JobRepository   crawler.JobRepository
+	ContentFilter   filter.CheckFn[*crawler.Content]
+	URLFilter       filter.CheckFn[*crawler.URL]
+	RelevanceFilter filter.CheckFn[*crawler.Content]
+}
+
+type Orchestrator struct {
+	frontier        frontier.Frontier
+	downloader      http.Downloader
+	parser          parser.Parser
+	urlRepository   crawler.URLRepository
+	jobRepository   crawler.JobRepository
+	contentFilter   filter.CheckFn[*crawler.Content]
+	urlFilter       filter.CheckFn[*crawler.URL]
+	relevanceFilter filter.CheckFn[*crawler.Content]
+}
+
+func NewOrchestrator(cfg Config) *Orchestrator {
+	return &Orchestrator{
+		frontier:        cfg.Frontier,
+		downloader:      cfg.Downloader,
+		parser:          cfg.Parser,
+		urlRepository:   cfg.URLRepository,
+		jobRepository:   cfg.JobRepository,
+		contentFilter:   cfg.ContentFilter,
+		urlFilter:       cfg.URLFilter,
+		relevanceFilter: cfg.RelevanceFilter,
+	}
+}
+
+func (o *Orchestrator) Run(ctx context.Context, seedURLs []string) error {
+	ctx, cancel := context.WithCancelCause(ctx)
+	for _, seedURL := range seedURLs {
+		parsed, err := crawler.ParseURL(seedURL, "")
+		if err != nil {
+			slog.Error("could not parse seed url", "err", err)
+			continue
+		}
+
+		err = o.urlRepository.Save(ctx, seedURL)
+		if err != nil {
+			slog.Error("error saving url", "err", err)
+			continue
+		}
+
+		err = o.frontier.AddURL(ctx, parsed)
+		if err == frontier.ErrMaxDomainLimit {
+			slog.Info("max domain limit reached, dropping new domains")
+			continue
+		}
+		if err != nil {
+			slog.Error("error adding seed url", "err", err)
+			continue
+		}
+
+	}
+
+	for {
+		nextURL, err := o.frontier.Next(ctx)
+		if err == frontier.ErrDone {
+			slog.Info("received Done signal. ending crawl")
+			cancel(frontier.ErrDone)
+			return nil
+		}
+		if err != nil {
+			slog.Error("error getting next url", "err", err)
+			cancel(err)
+			return err
+		}
+
+		rawHTML, err := o.downloader.Get(ctx, nextURL.RawURL)
+		if err != nil {
+			slog.Error("error downloading raw html", "err", err)
+			continue
+		}
+
+		content, err := o.parser.Parse(rawHTML.Content)
+		if err != nil {
+			slog.Error("error parsing content", "err", err)
+			continue
+		}
+
+		if err := o.contentFilter(content); err != nil {
+			slog.Info("content filtered out", "cause", err)
+			continue
+		}
+
+		if err := o.relevanceFilter(content); err == nil {
+			slog.Info("content passed relevance filter", "title", content.Title, "url", nextURL)
+			// TODO: add relevance pipeline. content has to be parsed for a job and saved to job db
+		}
+
+		for _, contentURL := range content.URLs {
+			parsed, err := crawler.ParseURL(nextURL.RawURL, contentURL)
+			if err != nil {
+				slog.Error("error parsing content url", "err", err, "url", contentURL)
+				continue
+			}
+			if err := o.urlFilter(&parsed); err != nil {
+				slog.Info("url filtered out", "cause", err)
+				continue
+			}
+			visited, err := o.urlRepository.Visited(ctx, parsed.RawURL)
+			if err != nil {
+				slog.Error("error querying URLRepository", "err", err, "url", parsed)
+				continue
+			}
+			if visited {
+				continue
+			}
+
+			err = o.urlRepository.Save(ctx, parsed.RawURL)
+			if err != nil {
+				slog.Error("error saving url", "err", err)
+				continue
+			}
+
+			err = o.frontier.AddURL(ctx, parsed)
+			if err == frontier.ErrMaxDomainLimit {
+				slog.Info("max domain limit reached, dropping new domains")
+				continue
+			}
+			if err != nil {
+				slog.Error("error adding url", "err", err)
+				continue
+			}
+
+		}
+
+		select {
+		case <-ctx.Done():
+			cancel(ctx.Err())
+			return ctx.Err()
+		default:
+		}
+	}
+}
